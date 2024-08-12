c7bc5b4e4854983be60cb5f4e86be54997e42025
QuirkySiiiiiid <jainjainja112233@gmail.com>
2024-08-12 12:53:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-#Thank you LazyDeveloper for helping me in this journey !
-#Must Subscribe On YouTube @LazyDeveloperr 
-
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return '@LazyDeveloper'
 
-
 if __name__ == "__main__":
-    app.run()
+    # Get the port from the environment variable, default to 5000 if not set
+    port = int(os.environ.get('PORT', 5000))
+    # Run the app on all network interfaces (0.0.0.0) and the specified port
+    app.run(host='0.0.0.0', port=port)
